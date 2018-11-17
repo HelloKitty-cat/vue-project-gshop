a69ba3c3f2807795e58c4a8d67041a417dd3371d
zhoubinhai <1987887482@qq.com>
2018-11-17 13:48:19 +0800

    init first

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <router-view />
+    <FooterGuide />
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+  export default {
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  html
+    height 100%
+    width 100%
+    background pink
+</style>
